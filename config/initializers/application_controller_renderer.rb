4b08ff6eeac236042d262025540b57f5441f5e87
shota-hibino <yamoushou@gmail.com>
2021-04-14 05:19:45 +0000

    [Add]image function create & comment function create

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '7339b9a99dd0428b9de2615c76effa039a392717cc60ce9f4ce653d72adfb2a11621278f5f106bcf9714a5f8946914a2dc188c7ae5ba57af03b858cc3b928211'
